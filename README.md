5636c246de822634651941e10a8a6fa717b05a18
gauravai2025 <21mc3018@rgipt.ac.in>
2023-11-07 19:40:04 +0530

    update

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Linear Regression Model
- 
+# Linear Regression Model in MATLAB
+
+This repository contains an implementation of linear regression in MATLAB. Linear regression is a machine learning algorithm used to predict a target variable based on one or more input features.
+
+Requirements
+<br>
+MATLAB
+ <img src="linear_screenshot.png" title="Model",width=400px>
+
+Usage
+<br>
+To use the linear regression model, simply open the linear_regression.m file in MATLAB and modify the input data and parameters as needed.
+
+Contributing
+<br>
+Contributions to this repository are always welcome. If you find a bug or have a suggestion for a new feature, please open an issue or submit a pull request.
---
+++ linear_regression.m
@@ -0,0 +1,50 @@
+x= 1:1:7;
+l1=length(x);
+y= [0.5,2.5,2.0,4.0,3.5,6.0,5.5];
+s1=0;
+for i=1:l1
+    s1=s1+x(i);
+end
+s2=0;s3=0;s4=0;
+for i=1:l1
+    s2=s2+y(i);
+    s3=s3+x(i)^2;
+    s4=s4+x(i)*y(i);
+end
+
+a1=(l1*s4-(s1*s2))/(l1*s3-s1^2);
+a0=s2/l1-a1*(s1/l1);
+scatter(x,y)  % draw only point
+hold on
+y1 = a0+a1*x; % linear regression model
+
+plot(x,y1);
+title('Linear Regression Model','color','r');
+xlabel(' X axis 1 <= x <= 7','color','m') ;
+ylabel(' Y axis 0.5<=y<=5.5','color','b') ;
+legend({'given data','Model'},'Location','Northeast')
+% sum= sum+sin(x(i));
+%y1=[0,0,0,0,0,0,0];
+for i=1:1:l1
+    y1(i)=a0+a1*x(i);
+    e =abs(y(i)-y1(i));
+    fprintf("error at data %d:%f\n",i,e);
+end
+   st=0;   % variance with respect data
+   sr=0;     % variance with respect to model
+y_avg=s2/7;
+
+for i=1:1:l1
+    st=st+(y(i)-y_avg)^2;
+    sr=sr+(y(i)-y1(i))^2;
+end
+ 
+ fprintf("variance with respect data:%f\n",st);
+ fprintf("variance with respect model:%f\n",sr);
+
+sy=(st/(l1-1))^1/2;         % standard deviation with respect data 
+sm= (sr/(l1-2))^1/2;     % standard deviation with respect to model
+r2=(st-sr)/st;
+fprintf("standard deviation with respect to given data:%f\n",sy);
+fprintf("standard deviation with respect to given model:%f\n",sm);
+fprintf("r2:%f\n",r2);
